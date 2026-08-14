d9e101429a571284c27b2686567dd315bb05452f
Nguyễn Tuấn <tuan-nv0505@MacBook-Pro-cua-Nguyen.local>
2026-08-14 23:00:05 +0700

    a

--- training/train.py
+++ training/train.py
@@ -235,7 +235,7 @@ def main(args):
                     torch.save(checkpoint, latest_path)
 
                     # checkpoint_manager.manage_local()
-                    if getattr(args.logging, "is_save_to_s3", False):
+                    if args.logging.is_save_to_s3:
                         checkpoint_manager.manage_s3(specific_checkpoint_path=checkpoint_path)
 
                     logger.info(f"Saved checkpoint to {checkpoint_path} and updated latest.pt")
