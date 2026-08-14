ab9b584bea3ec4aa57e0c9bb46286449efde621c
Nguyễn Tuấn <tuan-nv0505@MacBook-Pro-cua-Nguyen.local>
2026-08-15 00:13:35 +0700

    a

--- training/train.py
+++ training/train.py
@@ -74,7 +74,6 @@ def main(args):
 
         z = torch.randn(1, 4, latent_size, latent_size, device=device)
         fixed_z_cfg = torch.cat([z, z], dim=0)
-        print(os.environ.get("AWS_ACCESS_KEY_ID"))
         checkpoint_manager = CheckpointManager(
             checkpoint_dir=checkpoint_dir,
             aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
@@ -231,12 +230,12 @@ def main(args):
                     }
 
                     checkpoint_path = f"{checkpoint_dir}/checkpoint_{train_steps:07d}.pt"
-                    torch.save(checkpoint, checkpoint_path)
+                    # torch.save(checkpoint, checkpoint_path)
 
-                    latest_path = f"{checkpoint_dir}/checkpoint_latest.pt"
-                    torch.save(checkpoint, latest_path)
+                    # latest_path = f"{checkpoint_dir}/checkpoint_latest.pt"
+                    # torch.save(checkpoint, latest_path)
 
-                    checkpoint_manager.manage_local()
+                    # checkpoint_manager.manage_local()
                     if args.logging.is_save_to_s3:
                         checkpoint_manager.manage_s3(specific_checkpoint_path=checkpoint_path)
 
